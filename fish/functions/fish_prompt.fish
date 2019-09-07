aac9241c8a40c6f20d189c6e9faf7b05caad3076
Greg Allen <greg@firstandthird.com>
2019-09-07 12:25:59 -0700

    fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,1 +1,49 @@
-/data/data/com.termux/files/home/.local/share/omf/themes/cbjohnson/fish_prompt.fish
+function fish_prompt
+  # Cache exit status
+  set -l last_status $status
+
+  # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+  if not set -q __fish_prompt_char
+    switch (id -u)
+      case 0
+        set -g __fish_prompt_char \u276f\u276f
+      case '*'
+        set -g __fish_prompt_char »
+    end
+  end
+
+  # Setup colors
+  set -l normal (set_color normal)
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l bpurple (set_color -o purple)
+  set -l bred (set_color -o red)
+  set -l bcyan (set_color -o cyan)
+  set -l bwhite (set_color -o white)
+
+  # Configure __fish_git_prompt
+  set -g __fish_git_prompt_show_informative_status true
+  set -g __fish_git_prompt_showcolorhints true
+
+  # Color prompt char red for non-zero exit status
+  set -l pcolor $bpurple
+  if [ $last_status -ne 0 ]
+    set pcolor $bred
+  end
+
+  # Top
+  echo -n $cyan$USER$normal at $yellow$__fish_prompt_hostname$normal in $bred(prompt_pwd)$normal 
+  __fish_git_prompt
+
+  if [ "$AWS_PROFILE" != "" ]
+    echo "$yellow AWS:$AWS_PROFILE $AWS_DEFAULT_REGION $normal"
+  else
+    echo
+  end
+
+  # Bottom
+  echo -n $pcolor$__fish_prompt_char $normal
+end
